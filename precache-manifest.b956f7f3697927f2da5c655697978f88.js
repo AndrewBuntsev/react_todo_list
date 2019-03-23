464e77e27e8a6911912caf8524838d8d8023c050
ABuntsev <andrew.buntsev@gmail.com>
2019-03-23 13:06:47 +1100

    Updates

---
+++ precache-manifest.b956f7f3697927f2da5c655697978f88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a253a9bce0098e4b27d6",
+    "url": "/react_todo_list/static/js/runtime~main.920f6ad3.js"
+  },
+  {
+    "revision": "d6a2e146b56e9d3cb680",
+    "url": "/react_todo_list/static/js/main.3e585cf6.chunk.js"
+  },
+  {
+    "revision": "6c7e17ff5bf846b8a477",
+    "url": "/react_todo_list/static/js/2.ab41e63c.chunk.js"
+  },
+  {
+    "revision": "43c4f416c4561b6ad45ffc747bdd860a",
+    "url": "/react_todo_list/index.html"
+  }
+];
